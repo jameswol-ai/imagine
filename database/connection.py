87a8ee28b5c29125cadc7abcbae1f82b638457da
James Wol <james.wol@petalmail.com>
2026-08-19 01:36:21 +0300

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -1,10 +1,12 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.config import DATABASE_URL
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
+from app.settings import settings
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
-def get_db():
-    db = SessionLocal()
-    return db
+async def get_db() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
+
+Base = declarative_base()
