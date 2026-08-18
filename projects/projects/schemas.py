6feee786435780e4d0ec3f4090e616cf18ceb78f
James Wol <james.wol@petalmail.com>
2026-08-19 00:57:09 +0300

    Create projects/projects/schemas.py

---
+++ projects/projects/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class ProjectCreate(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+class ProjectOut(BaseModel):
+    id: int
+    name: str
+    description: Optional[str] = None
+    status: str
+
+    class Config:
+        orm_mode = True
