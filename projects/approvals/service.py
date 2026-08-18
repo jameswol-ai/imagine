aa8d984c7be11160c5e1036a9a28da61337c4de9
James Wol <james.wol@petalmail.com>
2026-08-19 01:00:18 +0300

    Create projects/approvals/service.py

---
+++ projects/approvals/service.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+from .models import Approval
+
+def create_approval(db: Session, project_id: int, approver_id: int, comment: str = None):
+    approval = Approval(project_id=project_id, approver_id=approver_id, comment=comment)
+    db.add(approval)
+    db.commit()
+    db.refresh(approval)
+    return approval
+
+def list_approvals(db: Session, project_id: int):
+    return db.query(Approval).filter(Approval.project_id == project_id).all()
