e88ac59608a9ccb8f17e430e90043665af5151da
James Wol <james.wol@petalmail.com>
2026-08-19 01:40:54 +0300

    Update utils.py

--- core/authentication/utils.py
+++ core/authentication/utils.py
@@ -1,20 +1,18 @@
-import time
-from typing import Dict
-import jwt
-from app.config import SECRET_KEY
+from datetime import datetime, timedelta
+from jose import jwt
+from app.settings import settings
+from passlib.context import CryptContext
 
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_SECONDS = 60 * 60 * 24  # 24 hours
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def create_access_token(subject: str, expires_delta: int = ACCESS_TOKEN_EXPIRE_SECONDS) -> str:
-    now = int(time.time())
-    payload: Dict[str, object] = {
-        "sub": subject,
-        "iat": now,
-        "exp": now + expires_delta
-    }
-    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
-    return token
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
-def decode_access_token(token: str) -> Dict:
-    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
