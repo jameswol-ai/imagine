03c902a586e5a0beeb5e7b3bb7abf148fb610ddf
James Wol <james.wol@petalmail.com>
2026-08-19 01:51:51 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,15 @@ from projects.projects.routes import router as projects_router
 from core.authentication.routes import router as auth_router
 # Import all routers from other modules...
 
+from architecture.routes import router as architecture_router
+from bim.routes import router as bim_router
+from structural.routes import router as structural_router
+
+app.include_router(architecture_router, prefix=settings.API_V1_PREFIX)
+app.include_router(bim_router, prefix=settings.API_V1_PREFIX)
+app.include_router(structural_router, prefix=settings.API_V1_PREFIX)
+
+
 app = FastAPI(
     title=settings.APP_NAME,
     version="1.0.0",
