e17dfdfbcc01b5793078a64b248b30a9ab01e83c
James Wol <james.wol@petalmail.com>
2026-08-19 09:44:33 +0300

    Update base.py

--- database/models/base.py
+++ database/models/base.py
@@ -1,13 +1,38 @@
-from sqlalchemy import Column, DateTime, String, func
-from database.connection import Base
+# IMAGINE/database/models/base.py
+
 import uuid
+
+from sqlalchemy import Column, DateTime, String, func
 from sqlalchemy.dialects.postgresql import UUID
+
+from . import Base
+
 
 class BaseModel(Base):
     __abstract__ = True
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    created_at = Column(DateTime, server_default=func.now())
-    updated_at = Column(DateTime, onupdate=func.now())
-    created_by = Column(String, nullable=True)
-    updated_by = Column(String, nullable=True)
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+
+    created_at = Column(
+        DateTime,
+        server_default=func.now(),
+    )
+
+    updated_at = Column(
+        DateTime,
+        onupdate=func.now(),
+    )
+
+    created_by = Column(
+        String,
+        nullable=True,
+    )
+
+    updated_by = Column(
+        String,
+        nullable=True,
+    )
