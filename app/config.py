13f8d874339d6f31d46091ea4caf70764b8ca730
James Wol <james.wol@petalmail.com>
2026-08-19 08:56:26 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,8 +1,15 @@
+"""
+IMAGINE
+Application Configuration
+"""
+
 from .settings import settings
+
 
 class AppConfig:
     API_PREFIX = settings.API_V1_PREFIX
     PROJECT_NAME = settings.APP_NAME
     DEBUG = settings.DEBUG
+
 
 config = AppConfig()
