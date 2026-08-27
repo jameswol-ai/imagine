644c4ac7c22f4832611aeb6616137d1d8307d822
James Wol <james.wol@petalmail.com>
2026-08-27 19:12:27 +0300

    Update schemas.py

--- projects/revisions/schemas.py
+++ projects/revisions/schemas.py
@@ -1,13 +1,19 @@
+from __future__ import annotations
+
+from uuid import UUID
+
 from pydantic import BaseModel
 
+
 class RevisionCreate(BaseModel):
-    project_id: int
+    project_id: UUID
     description: str
     created_by: int
 
+
 class RevisionOut(BaseModel):
     id: int
-    project_id: int
+    project_id: UUID
     description: str
     created_by: int
 
