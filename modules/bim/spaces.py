d79f371fa7e06420821d14cc8a056d1cac5fd584
James Wol <james.wol@petalmail.com>
2026-08-27 14:33:49 +0300

    Update spaces.py

--- modules/bim/spaces.py
+++ modules/bim/spaces.py
@@ -1,7 +1,7 @@
 """
 IMAGINE BIM Module
 
-Storey Service
+Space Service
 
 Version 24.1
 """
@@ -9,54 +9,72 @@ Version 24.1
 import uuid
 
 
-class StoreyService:
+class SpaceService:
 
     @staticmethod
-    def create_storey(
-        level,
-        height,
+    def create_space(
+        name,
+        space_type,
         area,
-        building_id
+        height,
+        storey_id
     ):
 
         return {
             "id": str(uuid.uuid4()),
-            "building_id": building_id,
-            "level": level,
-            "height": height,
-            "area": area
+            "storey_id": storey_id,
+            "name": name,
+            "space_type": space_type,
+            "area": area,
+            "height": height
         }
 
     @staticmethod
-    def update_storey(
-        storey,
+    def update_space(
+        space,
         updates
     ):
 
-        storey.update(updates)
+        space.update(updates)
 
-        return storey
+        return space
 
     @staticmethod
-    def delete_storey(
-        storey_id,
-        storeys
+    def delete_space(
+        space_id,
+        spaces
     ):
 
         return [
-            s for s in storeys
-            if str(s["id"]) != str(storey_id)
+            s for s in spaces
+            if str(s["id"]) != str(space_id)
         ]
 
     @staticmethod
-    def total_floor_area(
-        storeys
+    def total_space_area(
+        spaces
     ):
 
         return round(
             sum(
                 s.get("area", 0)
-                for s in storeys
+                for s in spaces
             ),
             2
+        )
+
+    @staticmethod
+    def occupancy_estimate(
+        spaces,
+        factor=10
+    ):
+
+        total_area = (
+            SpaceService.total_space_area(
+                spaces
+            )
+        )
+
+        return int(
+            total_area / factor
         )
