d00eb432cee938a5c0ac41647190ec4455b26cf7
James Wol <james.wol@petalmail.com>
2026-09-03 10:42:25 +0200

    BIM: add domain package

---
+++ modules/bim/__init__.py
@@ -0,0 +1,15 @@
+"""BIM domain modules for the IMAGINE AEC platform."""
+
+__all__ = [
+    "buildings",
+    "storeys",
+    "spaces",
+    "elements",
+    "assemblies",
+    "ifc_export",
+    "cobie",
+    "coordination",
+    "quantities",
+    "costing_handoff",
+    "digital_twin",
+]
