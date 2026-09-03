24f08d9fe78c8d286859c59266d7e789ca48d10e
James Wol <james.wol@petalmail.com>
2026-09-03 06:43:57 +0200

    Add architecture design standards package

---
+++ architecture/standards/__init__.py
@@ -0,0 +1,5 @@
+"""Architecture design standards reference and comparison tools."""
+
+from .engine import STANDARD_PROFILES, compare_standard_requirements, get_standard_profile
+
+__all__ = ["STANDARD_PROFILES", "compare_standard_requirements", "get_standard_profile"]
