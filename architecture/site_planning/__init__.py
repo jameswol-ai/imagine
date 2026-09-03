9b34afa33400d9f592eb1fa6d4cd14d4fed247fd
James Wol <james.wol@petalmail.com>
2026-09-03 07:51:30 +0200

    Export stable Site Planning interfaces

--- architecture/site_planning/__init__.py
+++ architecture/site_planning/__init__.py
@@ -1,1 +1,11 @@
+"""Site Planning domain package with stable public interfaces."""
 
+from .repository import SitePlanRepository, SitePlanningRepository
+from .service import SitePlanService, SitePlanningService
+
+__all__ = [
+    "SitePlanRepository",
+    "SitePlanningRepository",
+    "SitePlanService",
+    "SitePlanningService",
+]
