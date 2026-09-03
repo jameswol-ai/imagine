abb8d1a6facd0481ee5a9c04847f71a434215fb5
James Wol <james.wol@petalmail.com>
2026-09-03 06:18:54 +0200

    Add architecture assistant package

---
+++ architecture/assistant/__init__.py
@@ -0,0 +1,5 @@
+"""Architecture decision-support assistant for IMAGINE."""
+
+from .engine import ArchitectureBrief, ArchitectureAssistant, ArchitectureRecommendation
+
+__all__ = ["ArchitectureBrief", "ArchitectureAssistant", "ArchitectureRecommendation"]
